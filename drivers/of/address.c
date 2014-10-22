f00250b8dfbce82a359e4f66035dae1a3277547d
Xiaocheng Li <lix@codeaurora.org>
2014-10-22 09:20:06 +0530

    of/address: Add new API of_iomap_by_name and of_get_address_by_name
    Create new API of_iomap_by_name and of_get_address_by_name, which
    would read "reg-names" property and get the reg address by its
    name.
    
    Change-Id: I7ce30da5a7cbc29f8349641af97ac7ed16b4f506
    Signed-off-by: Xiaocheng Li <lix@codeaurora.org>

--- drivers/of/address.c
+++ drivers/of/address.c
@@ -752,6 +752,23 @@ static u64 of_translate_ioport(struct device_node *dev, const __be32 *in_addr,
 	return port;
 }
 
+const __be32 *of_get_address_by_name(struct device_node *dev, const char *name,
+		u64 *size, unsigned int *flags)
+{
+	int index;
+
+	if (!name)
+		return NULL;
+
+	/* Try to read "reg-names" property and get the index by name */
+	index = of_property_match_string(dev, "reg-names", name);
+	if (index < 0)
+		return NULL;
+
+	return of_get_address(dev, index, size, flags);
+}
+EXPORT_SYMBOL(of_get_address_by_name);
+
 static int __of_address_to_resource(struct device_node *dev,
 		const __be32 *addrp, u64 size, unsigned int flags,
 		const char *name, struct resource *r)
@@ -840,6 +857,22 @@ void __iomem *of_iomap(struct device_node *np, int index)
 	return ioremap(res.start, resource_size(&res));
 }
 EXPORT_SYMBOL(of_iomap);
+
+void __iomem *of_iomap_by_name(struct device_node *np, const char *name)
+{
+	int index;
+
+	if (!name)
+		return NULL;
+
+	/* Try to read "reg-names" property and get the index by name */
+	index = of_property_match_string(np, "reg-names", name);
+	if (index < 0)
+		return NULL;
+
+	return of_iomap(np, index);
+}
+EXPORT_SYMBOL(of_iomap_by_name);
 
 /*
  * of_io_request_and_map - Requests a resource and maps the memory mapped IO
