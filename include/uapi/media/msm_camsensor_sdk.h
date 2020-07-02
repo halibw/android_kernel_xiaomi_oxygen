d2663947ac002c4525250f2da7593c241604e082
dianlujitao <dianlujitao@lineageos.org>
2020-07-02 20:06:57 +0800

    msm: camera: disable CSID virtualization
    Change-Id: Ia0cd98411d14e306cefa8df6f980b125d7ea79bd
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- include/uapi/media/msm_camsensor_sdk.h
+++ include/uapi/media/msm_camsensor_sdk.h
@@ -51,8 +51,6 @@
 
 #define MSM_SENSOR_BYPASS_VIDEO_NODE    1
 
-#define SECURE_CAMERA
-
 enum msm_sensor_camera_id_t {
 	CAMERA_0,
 	CAMERA_1,
@@ -357,7 +355,6 @@ struct msm_camera_csid_params {
 	unsigned int csi_clk;
 	struct msm_camera_csid_lut_params lut_params;
 	unsigned char csi_3p_sel;
-	unsigned char is_secure;
 };
 
 struct msm_camera_csid_testmode_parms {
