5c9bdd775123ae7d98321c31d70603d3c513968c
Martijn Coenen <maco@android.com>
2020-08-21 14:25:44 +0200

    FROMGIT: binder: print warnings when detecting oneway spamming.
    The most common cause of the binder transaction buffer filling up is a
    client rapidly firing oneway transactions into a process, before it has
    a chance to handle them. Yet the root cause of this is often hard to
    debug, because either the system or the app will stop, and by that time
    binder debug information we dump in bugreports is no longer relevant.
    
    This change warns as soon as a process dips below 80% of its oneway
    space (less than 100kB available in the configuration), when any one
    process is responsible for either more than 50 transactions, or more
    than 50% of the oneway space.
    
    Signed-off-by: Martijn Coenen <maco@android.com>
    Signed-off-by: Martijn Coenen <maco@google.com>
    Acked-by: Todd Kjos <tkjos@google.com>
    Link: https://lore.kernel.org/r/20200821122544.1277051-1-maco@android.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 261e7818f06ec51e488e007f787ccd7e77272918
    git.kernel.org/pub/scm/linux/kernel/git/gregkh/char-misc.git/
    char-misc-next)
    
    Bug: 147795659
    Change-Id: Idc2b03ddc779880ca4716fdae47a70df43211f25
    (cherry picked from commit df2eac33e7648144b2d0930166e9ebb7492f5b05)

--- drivers/android/binder_alloc.h
+++ drivers/android/binder_alloc.h
@@ -40,6 +40,7 @@ struct binder_transaction;
  * @offsets_size:       size of array of offsets
  * @extra_buffers_size: size of space for other objects (like sg lists)
  * @data:               pointer to base of buffer space
+ * @pid:                pid to attribute the buffer to (caller)
  *
  * Bookkeeping structure for binder transaction buffers
  */
@@ -59,6 +60,7 @@ struct binder_buffer {
 	size_t offsets_size;
 	size_t extra_buffers_size;
 	void *data;
+	int    pid;
 };
 
 /**
@@ -128,7 +130,8 @@ extern struct binder_buffer *binder_alloc_new_buf(struct binder_alloc *alloc,
 						  size_t data_size,
 						  size_t offsets_size,
 						  size_t extra_buffers_size,
-						  int is_async);
+						  int is_async,
+						  int pid);
 extern void binder_alloc_init(struct binder_alloc *alloc);
 void binder_alloc_shrinker_init(void);
 extern void binder_alloc_vma_close(struct binder_alloc *alloc);
