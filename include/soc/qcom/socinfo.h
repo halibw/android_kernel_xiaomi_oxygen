2587f9dbd01f4f94c85b89ef18b27bb60e4c3b64
MrArtemSid <artem.sid.key@gmail.com>
2021-02-21 18:40:17 +0400

    msm: Add initial support for msm8940
    Add initial set of changes to build the
    msm8940 target. This implementation is based on
    snapshot of the msm8940 device tree from msm-4.9 with updates from 4.19 msm8917\msm8937 bring up.
    msm-4.9 tag: LA.UM.9.6.2.r1-04600-89xx.0

--- include/soc/qcom/socinfo.h
+++ include/soc/qcom/socinfo.h
@@ -100,6 +100,8 @@
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,msm8937")
 #define early_machine_is_msm8917()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,msm8917")
+#define early_machine_is_msm8940()	\
+	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,msm8940")
 #define early_machine_is_sdm439()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,sdm439")
 #define early_machine_is_sdm429()	\
@@ -153,6 +155,7 @@
 #define early_machine_is_bengalp_iot()	0
 #define early_machine_is_msm8937()	0
 #define early_machine_is_msm8917()	0
+#define early_machine_is_msm8940()	0
 #define early_machine_is_sdm439()	0
 #define early_machine_is_sdm429()	0
 #define early_machine_is_qm215()	0
@@ -204,6 +207,7 @@ enum msm_cpu {
 	MSM_CPU_BENGALP_IOT,
 	MSM_CPU_8937,
 	MSM_CPU_8917,
+	MSM_CPU_8940,
 	MSM_CPU_SDM439,
 	MSM_CPU_SDM429,
 	MSM_CPU_QM215,
