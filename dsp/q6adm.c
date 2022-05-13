a9553ac376214921d1859da9dd95dd988466f753
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-13 15:55:38 +0800

    techpack: audio: Import smart amp changes

--- dsp/q6adm.c
+++ dsp/q6adm.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2017 XiaoMi, Inc.
  */
 #include <linux/module.h>
 #include <linux/slab.h>
@@ -20,6 +21,7 @@
 #include <dsp/q6common.h>
 #include <ipc/apr.h>
 #include "adsp_err.h"
+#include <dsp/smart_amp.h>
 
 #define TIMEOUT_MS 1000
 
@@ -43,6 +45,10 @@
 #endif
 
 #define SESSION_TYPE_RX 0
+
+#ifdef CONFIG_SMART_AMP
+static struct afe_smartamp_get_calib *calib_resp;
+#endif
 
 /* ENUM for adm_status */
 enum adm_cal_status {
@@ -138,6 +144,38 @@ static int adm_get_parameters[MAX_COPPS_PER_PORT * ADM_GET_PARAMETER_LENGTH];
 static int adm_module_topo_list[MAX_COPPS_PER_PORT *
 				ADM_GET_TOPO_MODULE_INSTANCE_LIST_LENGTH];
 static struct mutex dts_srs_lock;
+
+#ifdef CONFIG_SMART_AMP
+int32_t smartamp_get_set(uint32_t param_id, int32_t length, uint8_t get_set, u8 *user_data)
+{
+	int32_t  ret = 0;
+	switch (get_set) {
+	case TAS_SET_PARAM:
+		pr_debug("smartAmp: set param");
+		ret = afe_smartamp_set_calib_data(param_id , (struct afe_smartamp_set_params_t *)user_data, length);
+		break;
+	case TAS_GET_PARAM: {
+		if (calib_resp == NULL) {
+			calib_resp = kzalloc(sizeof(struct afe_smartamp_get_calib), GFP_KERNEL);
+			if (calib_resp == NULL) {
+				pr_err("smartAmp : No memory\n");
+				return -ENOMEM;
+			}
+		}
+		pr_debug("smartAmp: get param");
+
+		ret = afe_smartamp_get_calib_data(calib_resp, param_id, AFE_SMARTAMP_MODULE);
+		memcpy(user_data , calib_resp->res_cfg.payload , length);
+		}
+		break;
+	default:
+		goto fail_cmd;
+	}
+
+fail_cmd:
+	return ret;
+}
+#endif
 
 void msm_dts_srs_acquire_lock(void)
 {
