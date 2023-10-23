1298529b70bb39f52c953be9abf8dfdd68ec4375
Haris Wu <bymark256@gmail.com>
2023-10-24 01:52:37 +0800

    Revert "rpm-smd: call msm-4.9 snapshot RPM-SMD driver"
    This reverts commit e8cdc7dffcc7679b91e71efe88fa644d637114ff.
    
    Devicetree node updated.
    
    Signed-off-by: Haris Wu <bymark256@gmail.com>

--- include/soc/qcom/rpm-smd.h
+++ include/soc/qcom/rpm-smd.h
@@ -1,15 +1,9 @@
-/* Copyright (c) 2012, 2014-2017, The Linux Foundation. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
  */
+
+#include <linux/platform_device.h>
 
 #ifndef __ARCH_ARM_MACH_MSM_RPM_SMD_H
 #define __ARCH_ARM_MACH_MSM_RPM_SMD_H
@@ -305,4 +299,5 @@ static inline int __init msm_rpm_driver_init(void)
 	return 0;
 }
 #endif
+
 #endif /*__ARCH_ARM_MACH_MSM_RPM_SMD_H*/
